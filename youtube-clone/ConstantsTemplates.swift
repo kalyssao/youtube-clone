b128a2e24473204ac1f5921f15ed3572ca20bc38
kalyssao <klysssa@icloud.com>
2020-06-12 13:41:01 +0000

    added constants template to replace constants

---
+++ youtube-clone/ConstantsTemplates.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplates.swift
+//  youtube-clone
+//
+//  Created by LVZ on 6/12/20.
+//  Copyright © 2020 LVZ. All rights reserved.
+//
+// Add your own key and uncomment this code.
+
+import Foundation
+/*struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
